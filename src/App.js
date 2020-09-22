0892996b94a4cf7beb2003a7e10f6e2b6beff279
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-22 20:54:41 +0800

    not rendering variable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Article from "./Article";
+import Rendering from "./Rendering";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Article userLogin="Sudah Login" />
+                <Rendering />
             </header>
         </div>
     );
