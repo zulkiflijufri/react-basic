6957f47d12d753eedb10d9e30dc2b26b35a99183
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-11-06 16:52:17 +0800

    hook useReducer

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,31 @@
 import React from "react";
 
+const countReducer = function (state, action) {
+    switch (action.type) {
+        case "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
 function About() {
+    const [counter, dispatch] = React.useReducer(countReducer, 0);
     return (
         <div>
             <h2>About Page</h2>
+            <p>Example hook useReducer</p>
+            <div>
+                <button onClick={(e) => dispatch({ type: "DECREMENT" })}>
+                    -
+                </button>
+                {counter}
+                <button onClick={(e) => dispatch({ type: "INCREMENT" })}>
+                    +
+                </button>
+            </div>
         </div>
     );
 }
