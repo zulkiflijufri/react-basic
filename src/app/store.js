ff38d75105f525dda83513ce8fca17d697981428
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-11-07 21:45:05 +0800

    redux devtools

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,4 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import gridReducer from "../features/Grid/reducer";
 
@@ -6,6 +6,11 @@ let rootReducer = combineReducers({
     grid: gridReducer,
 });
 
-let store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers =
+    (typeof window !== "undefined" &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
